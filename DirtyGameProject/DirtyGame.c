4bb430e8773fe606073d3a893b12f86efecfc4ba
Eric Guimarães <ericmguimaraes@gmail.com>
2015-09-03 00:22:09 -0400

    stuff

--- DirtyGameProject/DirtyGame.c
+++ DirtyGameProject/DirtyGame.c
@@ -10,9 +10,15 @@ typedef struct {
 	int deleted;
 } creature_type;
 
+enum Dirtyness {
+	clean_state = 0,
+	halfdirty_state = 1,
+	dirty_state = 2
+};
+
 typedef struct {
 	int id;
-    int state;
+    enum Dirtyness state;
     int north;
     int south;
     int east;
@@ -28,12 +34,6 @@ enum GameState {
         done
     };
 
-enum Dirtyness {
-	clean_state = 0,
-	halfdirty_state = 1,
-	dirty_state = 2
-};
-
 char * getCommand(char * action);
 creature_type * getPlayer(char *action, int number_creatures);
 room_type * getPlayerRoom(creature_type *creature, int number_rooms);
@@ -45,6 +45,7 @@ void add_creature(creature_type *creature, room_type *room);
 creature_type * getPC(int number_creatures);
 room_type * findRoomById(int id, int number_rooms);
 creature_type * findCreatureById(int id_creature, int number_creatures);
+void initCreaturesInRooms(int number_rooms);
 
 int respect = 40;
 room_type *pointer_rooms;
@@ -65,11 +66,14 @@ int main()
                 int i;
                 for(i=0; i < number_rooms; i++) {
 						pointer_rooms[i].id = i;
-                        scanf("%d %d %d %d %d", &pointer_rooms[i].state, &pointer_rooms[i].north, &pointer_rooms[i].south, &pointer_rooms[i].east, &pointer_rooms[i].west);
+						int state_int;
+                        scanf("%d %d %d %d %d", &state_int, &pointer_rooms[i].north, &pointer_rooms[i].south, &pointer_rooms[i].east, &pointer_rooms[i].west);
+                        pointer_rooms[i].state = state_int;
                 }
                 scanf("%d",&number_creatures);
                 //read creature type and location
 				pointer_creatures = (creature_type *)malloc(sizeof(creature_type)*number_creatures);
+				initCreaturesInRooms(number_rooms);
 				//creatureType is 0 for the PC, 1 for an animal, 2 for an NPC
                 for (i = 0; i < number_creatures; i++){
 					pointer_creatures[i].id = i;
@@ -86,8 +90,6 @@ int main()
                 scanf("%s", &action[0]);
                 char *command;
                 command = getCommand(action);
-                printf(command);
-                printf("\n");
                 creature_type *player;
                 player = (creature_type *)getPlayer(action, number_creatures);
                 room_type *player_room;
@@ -108,7 +110,9 @@ int main()
                     move("west", player_room, player);
                 } else if (strcmp(command, "exit") == 0){
                     currentState=exitState;
-                }
+                } else if (strcmp(command, "unit-tests") == 0) {
+					//TODO unitTests();
+				}
                 if(respect<1 || respect>80)
                     currentState=gameOverState;
                 break;
@@ -130,7 +134,7 @@ int main()
                 printf("Current State was not recognized \n");
         }
     }
-    printf("Good Bye! \n");
+    printf("Goodbye! \n");
     return 0;
 }
 
@@ -205,12 +209,86 @@ room_type * getPlayerRoom(creature_type *creature, int number_rooms){
     return findRoomById(((creature_type) *creature).room,number_rooms);
 };
 
-void look(room_type *room, creature_type *player){
+char* roomStateToString(enum Dirtyness state)
+{
+   switch (state)
+   {
+      case clean_state: return "clean";
+      case halfdirty_state: return "half-dirty";
+	  case dirty_state: return "dirty";
+   }
+   return NULL;
+}
 
+//sample content
+//Room 0, half-dirty, neighbors 1 to the south 2 to the west, contains:
+//animal 0
+//PC
+//human 2
+void look(room_type *room, creature_type *player){
+	int room_number = ((room_type) *room).id;
+	char* state = roomStateToString(((room_type) *room).state);
+	printf("Room %d, %s, neighbors ",room_number,state);
+	int counter = 0;
+	if(((room_type) *room).north != -1){
+		printf("%d to the north, ",((room_type) *room).north);
+		counter++;
+	}
+	if(((room_type) *room).south != -1){
+		printf("%d to the south, ",((room_type) *room).south);
+		counter++;
+	}
+	if(((room_type) *room).east != -1){
+		printf("%d to the east, ",((room_type) *room).east);
+		counter++;
+	}
+	if(((room_type) *room).west != -1){
+		printf("%d to the west, ",((room_type) *room).west);
+		counter++;
+	}
+	if(counter==0){
+		printf(": none, ");
+	}
+	printf("contains: \n");
+	int animal = 0 , pc = 0 , npc = 0, i ;
+	for(i=0; i < 10; i++){
+	 //   printf("%d\n",i);
+        if(room->creatures[i] != NULL){
+            if(room->creatures[i]->type == 0){
+                pc++;
+            }
+            if(room->creatures[i]->type == 1){
+                animal++;
+            }
+            if(room->creatures[i]->type == 2){
+                npc++;
+            }
+        }
+	}
+	printf("animal: %d \n",animal);
+	printf("PC: %d \n",pc);
+	printf("npc: %d \n",npc);
 };
 
-void add_creature(creature_type *creature, room_type *room){
+void initCreaturesInRooms(int number_rooms){
+    int i, j;
+    for(i=0;i<number_rooms;i++){
+        for(j=0;j<number_rooms;j++){
+            pointer_rooms[i].creatures[j] = malloc(sizeof(creature_type));
+        }
+    }
+}
 
+void add_creature(creature_type *creature, room_type *room){
+    printf("creature type %d",creature->type);
+    int i;
+    for(i=0; i < 10; i++){
+        if(room->creatures[i] == NULL){ //TODO after verify it in the right way it is good to go
+            printf("add creature %d\n", i);
+            room->creatures[i] = creature;
+            break;
+        }
+    }
 };
 
 void remove_creature(creature_type *creature, room_type *room){
